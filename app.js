8ad30d49763fd5485b1e661703da79cb0bae19e6
Piret Saar <piret.saar.003@khk.ee>
2021-11-25 11:10:06 +0200

    Added addBook LS functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //UI object
 const ui = new UI()
+const ls = new LS()
 
 // event elements
 const form = document.querySelector('form');
@@ -22,6 +23,8 @@ function addBook(event){
 
     //add book value to visual by UI object
     ui.addBook(book)
+    //add book to local storage
+    ls.addBook(book)
 
     titleInput.value = '';
     authorInput.value = '';
